1a3aaa8bc3c3edee9193b4e9a958f909e9f67f06
andrei-cc <razvan.bugheanu@insbaixcamp.cat>
2025-04-24 19:37:02 +0200

    Usuari i gestor biblioteca emb tots els metodes

--- GestorBiblioteca.java
+++ GestorBiblioteca.java
@@ -4,18 +4,40 @@ import java.util.List;
 
 public class GestorBiblioteca {
     private List<Prestec> prestecs;
+
     public GestorBiblioteca() {
         this.prestecs = new ArrayList<>();
     }
+
+    // Prestar llibre
     public void prestarLlibre(Usuari usuari, Llibre llibre) {
         if (!llibre.esPrestat()) {
-        llibre.prestar();
-        Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
-        prestecs.add(prestec);
-        usuari.afegirLlibre(llibre);
-        System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
+            llibre.prestar();
+            Prestec prestec = new Prestec(usuari, llibre, LocalDate.now());
+            prestecs.add(prestec);
+            usuari.afegirLlibre(llibre);
+            System.out.println(usuari.getNom() + " ha agafat el llibre: " + llibre.getTitol());
         } else {
-            System.out.println("Aquest llibre ja està prestat.");
+            System.out.println("No hi ha còpies disponibles del llibre: " + llibre.getTitol());
+        }
+    }
+
+    // Retornar llibre
+    public void retornarLlibre(Usuari usuari, Llibre llibre) {
+        if (usuari.getLlibresPrestats().contains(llibre)) {
+            usuari.retornarLlibre(llibre);
+            llibre.retornar();
+            System.out.println(usuari.getNom() + " ha retornat el llibre: " + llibre.getTitol());
+        } else {
+            System.out.println("Aquest usuari no té aquest llibre en préstec.");
+        }
+    }
+
+    // Mostrar préstecs actuals
+    public void mostrarPrestecs() {
+        System.out.println("Préstecs actuals:");
+        for (Prestec p : prestecs) {
+            System.out.println("- " + p.getUsuari().getNom() + " té el llibre: " + p.getLlibre().getTitol() + " des de " + p.getDataPrestec() + " fins a " + p.getDataRetorn());
         }
     }
 }
--- Usuari.java
+++ Usuari.java
@@ -4,13 +4,48 @@ import java.util.List;
 public class Usuari {
     private String nom;
     private List<Llibre> llibresPrestats;
-    public Usuari(String nom) {
-    this.nom = nom;
-    this.llibresPrestats = new ArrayList<>();
-}
-public String getNom() { return nom; }
-public List<Llibre> getLlibresPrestats() { return llibresPrestats; }
-public void afegirLlibre(Llibre llibre) { llibresPrestats.add(llibre); }
-public void retornarLlibre(Llibre llibre) { llibresPrestats.remove(llibre); }
 
+    public Usuari(String nom) {
+        this.nom = nom;
+        this.llibresPrestats = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<Llibre> getLlibresPrestats() {
+        return llibresPrestats;
+    }
+
+    public void afegirLlibre(Llibre llibre) {
+        llibresPrestats.add(llibre);
+    }
+
+    public void retornarLlibre(Llibre llibre) {
+        llibresPrestats.remove(llibre);
+    }
+
+    // Afegir mètodes per gestionar usuaris
+    public void modificarNom(String nouNom) {
+        this.nom = nouNom;
+    }
+
+    // Aquest mètode és només un exemple de com eliminar un llibre prestat
+    public void eliminarLlibre(Llibre llibre) {
+        llibresPrestats.remove(llibre);
+    }
+
+    // Mètode per llistar tots els llibres prestats per l'usuari
+    public void llistarLlibresPrestats() {
+        System.out.println("Llibres prestats per " + nom + ":");
+        for (Llibre llibre : llibresPrestats) {
+            System.out.println(llibre.getTitol());
+        }
+    }
+
+    // Mètode per cercar un llibre prestat específic
+    public boolean cercarLlibre(Llibre llibre) {
+        return llibresPrestats.contains(llibre);
+    }
 }
