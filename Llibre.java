6d908700f5846a40552d865b8c5fb512f628a98b
GuillemFdez <guillem.fernandez@insbaixcamp.cat>
2025-04-24 18:47:40 +0200

    Creació del Main

--- Llibre.java
+++ Llibre.java
@@ -1,12 +1,15 @@
 public class Llibre {
+    
     private String titol;
     private String autor;
     private boolean prestat;
+
     public Llibre(String titol, String autor) {
         this.titol = titol;
         this.autor = autor;
         this.prestat = false;
     }
+
     public String getTitol() { return titol; }
 
     public String getAutor() { return autor; }
